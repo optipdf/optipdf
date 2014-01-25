717c6e45e23050c957242800fe8adb25fa19f887
sakulstra <lukasstrassel@googlemail.com>
2014-01-25 20:49:24 +0100

    initial l18n i10n

--- beta/PdfOptimizer.php
+++ beta/PdfOptimizer.php
@@ -18,9 +18,9 @@ class PdfOptimizer extends AbstractOptimizer{
         ],
         'Language'=>['option'=>'deu'],
         'Format'=>['option'=>'a5'],
-        'Rotation'=>['option'=>null],
-        'Layout'=>['option'=>1]
-
+        'Rotation'=>['option'=>'0'],
+        'Layout'=>['option'=>'1'],
+        'Colormode'=>['option'=>'black_and_white']//|color_grayscale|mixed
     ];
 
     public function __construct($options = array()){
@@ -37,14 +37,14 @@ class PdfOptimizer extends AbstractOptimizer{
         //copy file to folder and rotate if needed
         $qFile = new File(ROOT.DS.APP_DIR.DS.'tmp'.DS.'queue'.DS.$this->options['Job']['id']);
         //###############################################
-        $result = parent::_exec('pdfinfo '.$qFile->pwd(),'pdfinfo');
-        if(isset($result['stdout'])&&!$result['stderr']){
-            //do awesome math stuff
-            debug($result['stdout']);
-        }
+//        $result = parent::_exec('pdfinfo '.$qFile->pwd(),'pdfinfo');
+//        if(isset($result['stdout'])&&!$result['stderr']){
+//            //do awesome math stuff
+//            debug($result['stdout']);
+//        }
         //rotation? format?...
         //###############################################
-        if($this->options['Rotation']['option']!=null){//TODO:
+        if($this->options['Rotation']['option']!='0'){
             $cmd = 'pdftk '.$qFile->pwd().' cat 1-'.$this->options['Rotation']['option'].' output '.$this->_path.$this->options['Job']['id'].'.pdf';
             $result = parent::_exec($cmd,'pdftk');
             if($result['stderr']){
@@ -56,8 +56,7 @@ class PdfOptimizer extends AbstractOptimizer{
         }
         $qFile->close();
         //convert pdf to tif
-        $cmd = 'gs -dNOPAUSE -dBATCH -sDEVICE=tiffg4 -r600 -o '.$this->_path.'gs-%04d.tif '.$this->_path.$this->options['Job']['id'].'.pdf';
-        debug($cmd);
+        $cmd = 'gs -dNOPAUSE -dBATCH -sDEVICE=tiff24nc -r600 -o '.$this->_path.'gs-%04d.tif '.$this->_path.$this->options['Job']['id'].'.pdf';
         $result = parent::_exec($cmd,'ghostscript');
         if($result['stderr']){
             debug($result);
@@ -67,41 +66,44 @@ class PdfOptimizer extends AbstractOptimizer{
         unlink($this->_path.$this->options['Job']['id'].'.pdf');
         //scantailor processing
         $scanDir = new Folder($this->_path.'scantailor'.DS,true,0755);
-        $cmd = 'scantailor-cli -v --enable-page-detection --output-dpi=300 --enable-fine-tuning --margins-top=10 --default-margins-top=10 --content-detection=aggressive --alignment-vertical=top --alignment-horizontal=center --white-margins=true --normalize-illumination=true --threshold=1 --layout='.$this->options['Layout']['option'].' --despeckle=normal '.$this->_path.'*.tif '.$scanDir->pwd();
+        $cmd = 'scantailor-cli -v --enable-page-detection --enable-fine-tuning --output-dpi=335 --margins=7 --default-margins=7 --alignment-vertical=top --alignment-horizontal=center --white-margins=true --normalize-illumination=true --tiff-compression=none --color-mode='.$this->options['Colormode']['option'].' --threshold=1 --layout='.$this->options['Layout']['option'].' --despeckle=normal '.$this->_path.'*.tif '.$scanDir->pwd();
         parent::_exec($cmd,'scantailor-cli');
         $files = $dir->find('.*\.tif',true);
         parent::_cleanUp($dir->pwd(),$files);
         $files = $scanDir->find('.*\.tif',true);
         parent::_moveTo($scanDir->pwd(),$dir->pwd(),$files);
         $scanDir->delete();
-        die();
         //tesseract tif to html
         $files = $dir->find('.*\.tif',true);
         foreach($files as $file){
             $file = new File($dir->pwd() . $file);
-            $cmd = 'tesseract '.$dir->pwd().$file->name.' '.$this->_path.$file->name()." ".'$$--options--$$'." hocr";//TODO:
-            parent::_exec($cmd,"tesseract");
+            $cmd = 'tesseract '.$dir->pwd().$file->name.' '.$this->_path.$file->name()." -psm 1 -l ".$this->options['Language']['option']." hocr";
+            $result = parent::_exec($cmd,"tesseract");
+            if($result['return']!=0){
+                echo "some error inside tesseract";
+            }
             $file->close();
         }
-        //hocr2pdf html to pdf
-        $files = $dir->find('.*\.html',true);
-        foreach($files as $file){
-            $file = new File($dir->pwd() . DS . $file);
-            $cmd = 'hocr2pdf -s -i '.$this->_path.$file->name().".tif -o ".$this->_path.$file->name().".pdf < ".$this->_path.$file->name;
-            parent::_exec($cmd,"hocrtopdf");
-            $file->close();
-        }
-        //combine pdfs
-        $cmd = 'pdftk '.$this->_path.'*.pdf output '.$this->_path.'uncompressed-'.'$$--$filename--$$';//TODO:
-        parent::_exec($cmd,"pdftk");
-        $files = $dir->find('gs.*');
-        parent::_cleanUp($dir->pwd());
-        //compression
-        $cmd = 'gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dPDFFitPage -sPAPERSIZE='.SIZEOPTION.' -dCompatibilityLevel=1.4 -dEmbedAllFonts=true -dSubsetFonts=true -sOutputFile='.$this->_path.__FILENAME__.' '.$this->_path.'*.pdf';
-        $result = $this->_exec($cmd,"compress");
+        $cmd = 'pdfbeads -d '.$this->_path.'*.tif > '.$this->_path.$this->options['Job']['filename'];
+        $result = parent::_exec($cmd,"pdftk");
+        debug($result);
         //exif info
-
-
+        $options = '';
+        if($this->options['Job']['title']!='')
+            $options .= '-Title="'.$this->_path.$this->options['Job']['title'].'" ';
+        if($this->options['Job']['author']!='')
+            $options .= '-Author="'.$this->_path.$this->options['Job']['author'].'" ';
+        if($options != ''){
+            $cmd = 'exiftool -overwrite_original '.$options.'-Subject="optimzed by OptiPdf.de" '.$this->_path.$this->options['Job']['filename'];
+            $result = $this->_exec($cmd,"exiftool");
+            debug($result);
+        }
+        //cleanup the mess
+        $fdir = new Folder($this->_path.'../../finished/',true,0755);
+        $file = new File($this->_path.$this->options['Job']['filename']);
+        $file->copy($fdir->pwd().$this->options['Job']['id'].'_'.$this->options['Job']['filename']);
+        $dir->delete();
+        return;
     }
 
 } 
